944a711856c39a537743a24efd1caa6e908ab24a
Unknown <punto.damar2@gmail.com>
2019-02-07 15:05:35 +0700

    export game

---
+++ docs/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1549526628,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/faces1-sheet0.png",
+		"images/faces1-sheet1.png",
+		"images/faces1-sheet2.png",
+		"images/tiledbackground2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"percakapan.xml"
+	]
+}
