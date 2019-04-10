7e74810ddf28a200f18afc86065340cd5b02f246
Daniel William <arcadesindo@gmail.com>
2019-04-10 11:58:21 +0700

    Update Data
    Mengubah gambar dan menambah sistem touch

--- docs/offline.js
+++ docs/offline.js
@@ -1,14 +1,13 @@
 ﻿{
-	"version": 1549544585,
+	"version": 1554872062,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
 		"images/tiledbackground.png",
-		"images/faces1-sheet0.png",
-		"images/faces1-sheet1.png",
-		"images/faces1-sheet2.png",
+		"images/faces-sheet0.png",
+		"images/faces2-sheet0.png",
 		"images/tiledbackground2.png",
 		"icon-16.png",
 		"icon-32.png",
